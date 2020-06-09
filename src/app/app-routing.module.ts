5327a7f2d681d780a412cfb2bcc1ae5e70c08197
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-06-09 12:18:52 +0530

    All required compoenents added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,42 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'places',
+    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
+  },
+  {
+    path: 'discover',
+    loadChildren: () => import('./places/discover/discover.module')
+  },
+  {
+    path: 'place-detail',
+    loadChildren: () => import('./places/discover/place-detail/place-detail.module')
+  },
+  {
+    path: 'offer',
+    loadChildren: () => import('./places/offer/offer.module')
+  },
+  {
+    path: 'edit-offer',
+    loadChildren: () => import('./places/offer/edit-offer/edit-offer.module')
+  },
+  {
+    path: 'new-offer',
+    loadChildren: () => import('./places/offer/new-offer/new-offer.module')
+  },
+  {
+    path: 'offer-bookings',
+    loadChildren: () => import('./places/offer/offer-bookings/offer-bookings.module')
+  },
+  {
+    path: 'bookings',
+    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
+  },
 ];
 
 @NgModule({
