6e28414f6c6c47de79693db8e7235d48e3e68b3c
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-06-09 13:02:58 +0530

    Navigation tabs made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo : 'places',
     pathMatch: 'full'
   },
   {
@@ -18,30 +14,6 @@ const routes: Routes = [
   {
     path: 'places',
     loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./places/discover/discover.module')
-  },
-  {
-    path: 'place-detail',
-    loadChildren: () => import('./places/discover/place-detail/place-detail.module')
-  },
-  {
-    path: 'offer',
-    loadChildren: () => import('./places/offer/offer.module')
-  },
-  {
-    path: 'edit-offer',
-    loadChildren: () => import('./places/offer/edit-offer/edit-offer.module')
-  },
-  {
-    path: 'new-offer',
-    loadChildren: () => import('./places/offer/new-offer/new-offer.module')
-  },
-  {
-    path: 'offer-bookings',
-    loadChildren: () => import('./places/offer/offer-bookings/offer-bookings.module')
   },
   {
     path: 'bookings',
--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -5,16 +5,54 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./discover/place-detail/place-detail.module').then( m => m.PlaceDetailPageModule)
+          }
+        ]
+      },
+      {
+        path: 'offer',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./offer/offer.module').then( m => m.OfferPageModule)
+          },
+          {
+            path: 'new',
+            loadChildren: () => import('./offer/new-offer/new-offer.module').then(m => m.NewOfferPageModule)
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () => import('./offer/edit-offer/edit-offer.module').then(m => m.EditOfferPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./offer/offer-bookings/offer-bookings.module').then(m => m.OfferBookingsPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: './places/tabs/discover',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
     path: '',
-    component: PlacesPage
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offer',
-    loadChildren: () => import('./offer/offer.module').then( m => m.OfferPageModule)
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full'
   }
 ];
 
